8636d2e28e88ba0b94a874af62f187348033359e
KononOleg <konon.oleg97@gmail.com>
2023-03-09 16:09:06 +0300

    fix: validation

--- src/pages/authorization/components/third-registration/third-registration.tsx
+++ src/pages/authorization/components/third-registration/third-registration.tsx
@@ -11,46 +11,50 @@ interface IProps {
   isValid: boolean;
 }
 
-export const ThirdRegistration: FC<IProps> = ({ register, errors, isValid }) => (
-  <Fragment>
-    <div className='fields'>
-      <div className='input-container'>
+export const ThirdRegistration: FC<IProps> = ({ register, errors, isValid }) => {
+  console.log(errors.phone?.type);
+  return (
+    <Fragment>
+      <div className='fields'>
+        <div className='input-container'>
+          <TextInput
+            label='Номер телефона'
+            mask='+375 (99) 999-99-99'
+            maskChar='x'
+            isError={errors.phone}
+            register={{
+              ...register('phone', {
+                required: 'Поле не может быть пустым',
+                pattern: regex.phone,
+              }),
+            }}
+            error={errors.phone}
+          />
+          {errors.phone?.type === 'pattern' && (
+            <p className={`error info_large ${errors.phone ? 'color_error' : ''}`} data-test-id='hint'>
+              В формате +375 (xx) xxx-xx-xx
+            </p>
+          )}
+        </div>
         <TextInput
-          label='Номер телефона'
-          mask='+375 (99) 999-99-99'
-          maskChar='x'
-          isError={errors.phone}
+          label='E-mail'
+          isError={errors.email}
           register={{
-            ...register('phone', {
+            ...register('email', {
               required: 'Поле не может быть пустым',
-              pattern: regex.phone,
+              pattern: {
+                value: regex.email,
+                message: 'Введите корректный e-mail',
+              },
             }),
           }}
-          error={errors.phone}
+          error={errors.email}
         />
-
-        <p className={`error info_large ${errors.phone ? 'color_error' : ''}`} data-test-id='hint'>
-          В формате +375 (xx) xxx-xx-xx
-        </p>
       </div>
-      <TextInput
-        label='E-mail'
-        isError={errors.email}
-        register={{
-          ...register('email', {
-            required: 'Поле не может быть пустым',
-            pattern: {
-              value: regex.email,
-              message: 'Введите корректный e-mail',
-            },
-          }),
-        }}
-        error={errors.email}
-      />
-    </div>
 
-    <button className='button' type='submit' disabled={!isValid}>
-      Зарегистрироваться
-    </button>
-  </Fragment>
-);
+      <button className='button' type='submit' disabled={!isValid}>
+        Зарегистрироваться
+      </button>
+    </Fragment>
+  );
+};
